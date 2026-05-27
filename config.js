1ece8383652eaa53fec3ca84d45f23761f5455fc
gisli-collab <gisli@aha.is>
2026-05-27 11:59:15 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 window.STORE_DISPLAY_CONFIG = {
-  storeName: 'Store Product Display',
+  storeName: 'display-app V1.2',
 
-  // Manual CSV mode:
-  // Replace data/products.csv in GitHub whenever you want to publish new product data.
-  dataSource: './data/products.csv',
+  // Manual upload mode:
+  // Leave dataSource empty so no product CSV is hardcoded into GitHub/Netlify.
+  // Users upload a CSV in the browser, edit locally, then download the updated CSV.
+  dataSource: '',
 
   currency: 'ISK',
   locale: 'is-IS',
   priceField: 'cost',
   imageField: 'img',
   barcodeField: 'barcodex',
+  shelfCodeField: 'shelfcode',
   defaultSort: 'name-asc'
 };
